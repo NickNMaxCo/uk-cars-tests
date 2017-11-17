53e23301da523e60b27a467f3c360a5cabd5dac7
max <maksistenz@gmail.com>
2017-11-17 19:39:06 +0200

    Help me choose feature(changes after first pull request - third edition)

--- pages/advice_video_page.rb
+++ pages/advice_video_page.rb
@@ -3,7 +3,7 @@ require_relative 'page'
 class AdviceVideoPage < Page
 
   def marker
-    page.find(:xpath, "//h1[@class = 'display-1']", visible: false).text
+    page.find("h1[class = 'display-1']", visible: false).text
   end
 
 end
