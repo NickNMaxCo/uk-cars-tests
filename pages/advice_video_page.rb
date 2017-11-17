4f2276998adeae7321eb74dc4538193118eb4c26
max <maksistenz@gmail.com>
2017-11-17 20:08:21 +0200

    Help me choose feature(changes after first pull request - fourth edition)

--- pages/advice_video_page.rb
+++ pages/advice_video_page.rb
@@ -3,7 +3,7 @@ require_relative 'page'
 class AdviceVideoPage < Page
 
   def marker
-    page.find("h1[class = 'display-1']", visible: false).text
+    page.find("h1.display-1", visible: false).text
   end
 
 end
